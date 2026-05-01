c4fe62f818674aba26867395f7df53a8d52da055
Tony Lialin <tlialin@gmail.com>
2026-05-01 01:25:54 -0700

    Add Render.com config

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 import json
 
-PORT = int(os.environ.get('PORT', 3000))
+PORT = int(os.environ.get('PORT', 10000))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
